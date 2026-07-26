bb4cbc0e88d71ab87192a2e390cfb4a746a92a0a
Dossomecoulibaly19-hub <kikicoulibaly19@gmail.com>
2026-07-26 09:58:41 +0000

    Update app.py

--- app.py
+++ app.py
@@ -200,4 +200,4 @@ def handle_send(data):
     emit('new_message_alert',{},room=data['to'])
 
 if __name__=='__main__':
-    socketio.run(app,host='0.0.0.0',port=int(os.environ.get("PORT", 10000)))
+    socketio.run(app,host='0.0.0.0',port=int(os.environ.get("PORT", 10000)), allow_unsafe_werkzeug=True)
